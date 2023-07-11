86011b6a2aed8319cfd6be80d6cd6bb6008ec15f
Jenaide Sibolie <jenaidesibolie@gmail.com>
2023-07-11 21:07:30 +0200

    secret key

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-^9jv34vt73i9sa9$a62lcm3ryh9l8s@0kos&y4gvjagly13h@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
