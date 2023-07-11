287a327ae9932f9c7fc3df6c1a6e18836d0c9fc1
Jenaide Sibolie <jenaidesibolie@gmail.com>
2023-07-11 22:33:22 +0200

    added comments

--- todo/form.py
+++ todo/form.py
@@ -2,6 +2,7 @@ from django import forms
 
 # Reordering Form and View
 
-
+"""" sets up a form called PositionForm with a single field called 
+"position" where users can input the positions of tasks for reordering """
 class PositionForm(forms.Form):
     position = forms.CharField()
--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-
+# a basic Task model that can be used to create a new Task 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
